867c9941368c8ede8877e99cf9ffa9f47afbb074
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-04-24 19:26:38 -0600

    added 4 dimentions

--- treasure_map.py
+++ treasure_map.py
@@ -1,8 +1,9 @@
-row1 = [" "," "," "]     # means : index 0
-row2 = [" "," "," "]     # means : index 1
-row3 = [" "," "," "]     # means : index 3
+row1 = [" ", " ", " ", " "]     # means : index 0
+row2 = [" ", " ", " ", " "]     # means : index 1
+row3 = [" ", " ", " ", " "]     # means : index 3
+row4 = [" ", " ", " ", " "] 
 
-map = [row1,row2,row3]
+map = [row1,row2,row3, row4]
 
 position = input (" Where do you keep your treasure ? ") 
 # if 23 - that string data type : means column 2, row 3 - index 0 is 2; index 1 is 3
@@ -17,5 +18,5 @@ vertical =  int(position [1])   # use int to convert this string into integer
 
 map[vertical-1][horizontal-1] = "X"
 
-print(f"{row1}\n{row2}\n{row3}")
+print(f"{row1}\n{row2}\n{row3}\n{row4}")
 
